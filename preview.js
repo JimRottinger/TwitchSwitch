07c94a99d21752301144ad259ece24918d801b16
James Rottinger <jamesrottinger@gmail.com>
2014-02-24 22:17:20 -0500

    commented out some unused code

--- preview.js
+++ preview.js
@@ -22,9 +22,11 @@ var get_username = function(){
 	return $("#you .username").text();
 };
 
+//not currently being used but might when moving off of embed.ly. Do not remove
+/*
 var generate_embed = function(channel){
 	return "<object type='application/x-shockwave-flash' height='378' width='620' id='live_embed_player_flash' data='http://www.twitch.tv/widgets/live_embed_player.swf?channel="+channel+"' bgcolor='#000000'><param name='allowFullScreen' value='true' /><param name='allowScriptAccess' value='always' /><param name='allowNetworking' value='all' /><param name='movie' value='http://www.twitch.tv/widgets/live_embed_player.swf' /><param name='flashvars' value='hostname=www.twitch.tv&channel="+channel+"&auto_play=true&start_volume=25' /></object>";
-};
+};*/
 
 var popup_video = function(loc){
 	console.log("enabling popup");
