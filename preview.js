482ffa97f27ae418e5b403bac326bfba8d9851e7
Glen Oakley <goakley123@gmail.com>
2014-02-25 00:49:41 -0500

    Removed embedly

--- preview.js
+++ preview.js
@@ -35,37 +35,22 @@ function get_username() {
     return elements[0].innerText;
 }
 
-//not currently being used but might when moving off of embed.ly. Do not remove
-/*
 var generate_embed = function(channel){
-	return "<object type='application/x-shockwave-flash' height='378' width='620' id='live_embed_player_flash' data='http://www.twitch.tv/widgets/live_embed_player.swf?channel="+channel+"' bgcolor='#000000'><param name='allowFullScreen' value='true' /><param name='allowScriptAccess' value='always' /><param name='allowNetworking' value='all' /><param name='movie' value='http://www.twitch.tv/widgets/live_embed_player.swf' /><param name='flashvars' value='hostname=www.twitch.tv&channel="+channel+"&auto_play=true&start_volume=25' /></object>";
-};*/
+	return "<object type='application/x-shockwave-flash' height='189' width='310' id='live_embed_player_flash' data='http://www.twitch.tv/widgets/live_embed_player.swf?channel="+channel+"' bgcolor='#000000'><param name='allowFullScreen' value='true' /><param name='allowScriptAccess' value='always' /><param name='allowNetworking' value='all' /><param name='movie' value='http://www.twitch.tv/widgets/live_embed_player.swf' /><param name='flashvars' value='hostname=www.twitch.tv&channel="+channel+"&auto_play=true&start_volume=0' /></object>";
+};
 
 /** Pops up the video related to the myserious 'loc' object */
 var popup_video = function(loc){
-	console.log("enabling popup");
 	$(".popup").remove();
 	var offset = loc.offset().top;
 	var url = "http://twitch.tv/"+loc.data('channel_name');
 
-	var box = 	"<div class='popup' style='position: absolute;padding: 20px 20px;border: 2px solid #333;background: #fff;left: 20px;top:"+offset+"px; z-index: 5;overflow: visible;'> \
-	 				<h3> "+loc.data("channel_name")+" (Preview) <span class='xout' style='float:right; cursor: pointer;'> X </span></h3> \
-	 				<div id='player'></div> \
-	 				<p> <a href='"+url+"'> Switch to this stream </a> </p> \
+	var box = 	"<div class='popup' style='position: absolute;padding:12px;border:2px solid #333;background:#fff;left:20px;top:"+offset+"px; z-index:5;overflow:visible;'> \
+	 				<span class='xout' style='position:absolute;top:1ex;right:1ex;font-weight:bold;'>X</span>\
+	 				<h3><a href="+url+">"+loc.data("channel_name")+"</a> (Preview)</h3> \
+	 				<div id='player'>"+ generate_embed(loc.data('channel_name')) +"</div> \
 	 			</div>";
 	$("#main_col").append(box);
-
-	var maxwidth = $('#popup div').width();
-
-	console.log(url);
-
-	$.embedly.oembed(url, {query: {maxwidth: maxwidth, autoplay: true}})
-		.progress(function(obj){
-			console.log(obj);
-			$(".popup div").html(obj.html);			
-		});
-
-	return false;
 };
 
 // popup a video when a preview_link is clicked
